aeaf0de202e2221e7621590afc2b91ce86e486cb
ParkYeonJun <pyj8127@gmail.com>
2016-07-27 23:27:08 +0900

    해커톤 첫 날한 부분까지

--- test_crawl_1.py
+++ test_crawl_1.py
@@ -1,8 +1,47 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'programming.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crawl.settings')
 import django
 django.setup()
 
 from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
+
+from university.models import Graduate_school
+
+def main():
+    url_set = { gs.url for gs in Graduate_school.objects.all() }
+
+    graduate_school_list = []
+
+    for page in range(1, 10000):
+        params = {
+            'field_resetype_value': 'E',
+            'page': page,
+        }
+
+        page_url = 'http://eng.snu.ac.kr/research'
+        html = requests.get(page_url, params=params).text
+        soup = BeautifulSoup(html, 'html.parser')
+        print(soup.findAll("a"))
+        for a_tag in soup.findAll('a'):
+            title = a_tag.text
+            link = urljoin(page_url, a_tag['href'])
+
+            if link in url_set:
+                print('End!')
+                return graduate_school_list
+
+            url_set.add(link)
+
+            print(title, link)
+            graduate_school_list.append(Graduate_school(title=title, url=link))
+
+if __name__ == '__main__':
+    # from django.db import connection
+
+    graduate_school_list = main()
+    Graduate_school.objects.bulk_create(graduate_school_list)
+
+    # for idx, query in enumerate(connection.queries, 1):
+        # print(idx, query)
--- university/models.py
+++ university/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Graduate_school(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+
