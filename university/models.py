03a36920f9e17732307bd0c95a3e5b0d075cbb50
ParkYeonJun <pyj8127@gmail.com>
2016-08-01 21:16:33 +0900

    models 수정 완료

--- university/models.py
+++ university/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Graduate_school(models.Model):
     title = models.CharField(max_length=50, verbose_name="연구실 이름")
-    major = models.CharField(max_length=20, verbose_)
+    major = models.CharField(max_length=20)
     professor = models.CharField(max_length=20)
     location = models.CharField(max_length=20, help="ㅇㅇ동 ㅇㅇㅇ호")
     telephone = models.IntegerField(max_length=11, validators=[])
