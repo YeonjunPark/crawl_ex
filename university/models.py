d14894e1a34d746ffee668f2c49c554560969221
ParkYeonJun <pyj8127@gmail.com>
2016-08-01 21:25:50 +0900

    박연준

--- university/models.py
+++ university/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Graduate_school(models.Model):
-    title = models.CharField(max_length=50, verbose_name="연구실 이름")
-    major = models.CharField(max_length=20)
-    professor = models.CharField(max_length=20)
-    location = models.CharField(max_length=20, help="ㅇㅇ동 ㅇㅇㅇ호")
-    telephone = models.IntegerField(max_length=11, validators=[])
+    title = models.CharField(max_length=50, verbose_name="연구실 이름", null=True)
+    major = models.CharField(max_length=20, null=True)
+    professor = models.CharField(max_length=20, null=True)
+    location = models.CharField(max_length=20, help_text="ㅇㅇ동 ㅇㅇㅇ호", null=True)
+    telephone = models.CharField(max_length=11, null=True)
     url = models.URLField(blank=True, null=True)
 
-
+class Quest(models.Model):
+    name = models.CharField(max_length=100, null=True)
